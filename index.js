424d6e54b457d28d8b80ea1a26ef8ff0dd17583e
Darrin Barker <darrin.barker@flatironschool.com>
2023-02-17 17:30:24 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  //let superbowlWin = record.find(record => record.result === 'W')
+
+  //function superbowlWin(record){
+    //return (record === "W")
+
+    function superbowlWin(record){
+      let winTrack = record.find(record => record.result === 'W')
+        if (winTrack){
+          return (winTrack.year)
+        }
+        else return undefined
+        //return (winTrack.year)
+      
+    }
